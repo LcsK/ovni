93f87e276aaa23ab7ce64ab46bac1e9dddb3615c
LcsK <lucasaferrlopes@gmail.com>
2018-12-06 01:07:53 -0200

    fix database env

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://vsuozmqspjulbt:c81bf1b5b94f0eaf11e74afd014106493a2d28c5bf2d0e4078be7499733c59cc@ec2-54-204-40-248.compute-1.amazonaws.com:5432/d9uu38guc91upf',
+  connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
